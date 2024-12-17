9aa57ac563574e1ad9eee7b2c4aa99293c8da303
Peter Veshi <peterveshi@gmail.com>
2024-12-17 05:22:46 +0100

    spaceship up and moving.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import * 
+from player import *
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 100
         
         
     
